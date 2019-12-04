d196fd0e229582232fe1a379ca4ee4b03fafcb18
daikiante <daikiante@gmail.com>
2019-12-04 14:36:35 +0530

    2019/12/4

--- crud_main/first/urls.py
+++ crud_main/first/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
+
+
 urlpatterns = [
+    path('lohit', views.home),
+    path('about', views.about)
 ]
--- crud_main/first/views.py
+++ crud_main/first/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('My name is Lohit')
+
+def about(request):
+    return HttpResponse('I Love Robots')
