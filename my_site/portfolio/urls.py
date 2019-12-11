85373a1db802e7ca476a9f2aa1bfd018ec6a28f7
daikiante <daikiante@gmail.com>
2019-12-11 16:44:29 +0530

    2019/12/11

--- my_site/portfolio/urls.py
+++ my_site/portfolio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_index, name='index')
+    path('', views.project_index, name='index'),
+    path('<int:pk>/', views.project_detail, name='project_detail'),
 ]
--- my_site/portfolio/views.py
+++ my_site/portfolio/views.py
@@ -6,5 +6,12 @@ def project_index(request):
     context = {
         'projects':projects
     }
+    return render(request, 'portfolio/project_index.html', context)
 
-    return render(request, 'projects/project_index.html', context)
+def project_detail(request, pk):
+    # pk はプライマリーキーの事 IDと同じ意味
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'portfolio/project_detail.html', context)
