a898113819bf2ffe15229272a17ab32dde531c49
daikiante <daikiante@gmail.com>
2019-12-21 17:17:41 +0530

    2019/12/21

--- django_commerce/cart/models.py
+++ django_commerce/cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from products.models import Product
+
+
+User = get_user_model()
+
+class Cart(models.Model):
+    # Userとつなげる
+    user = models.ForeignKey(user.User, on_delete=models.CASCADE)
+
+    # プロダクトとつなげる
+    item = models.ForeignKey(item.Product, on_delete=models.CASCADE)
+
+    # カウントするためのフィールド
+    quantity = models.IntegerField(default=1)
+
+    # 作った時間
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.quantity} of {self.item.name}'
